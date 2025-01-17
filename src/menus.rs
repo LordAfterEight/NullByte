d507d122f1b72c6409d9f9089ffd2932c722916d
LordAfterEight <elias@stettmayer.de>
2025-01-17 09:44:39 +0100

    Added files I forgot to add in previous commit

---
+++ src/menus.rs
@@ -0,0 +1,48 @@
+use std::{io,thread,time};
+use ratatui::{
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    style::Stylize,
+    widgets::Paragraph,                                                                    DefaultTerminal,
+};
+
+pub fn main_menu(mut terminal: DefaultTerminal) -> io::Result<()> {
+    loop {
+        terminal.draw(|frame| {
+            let menu_ui = Paragraph::new("
+            Build: 17.01.2025, made by Elias Stettmayer
+
+            ┏━━━━━━━━━━━━━━━━━━━━━━ CLI-Miner ━━━━━━━━━━━━━━━━━━━━━┓
+            ┃ Exit [q]                                             ┃
+            ┃                                                      ┃
+            ┃           Press [Enter] to start the game            ┃
+            ┃                                                      ┃
+            ┃           Press [e] to open settings                 ┃
+            ┃                                                      ┃
+            ┃                                                      ┃
+            ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+            ");
+            let menu_display = menu_ui
+                .white()
+                .on_black();
+            frame.render_widget(menu_display, frame.area());
+        })?;
+
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(());
+            }
+
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('e') {
+                println!("Settings opened");
+                thread::sleep(time::Duration::from_millis(1000));
+                return Ok(());
+            }
+
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('\n') {
+                println!("Enter pressed");
+                thread::sleep(time::Duration::from_millis(1000));
+                return Ok(());
+            }
+        }
+    }
+}
