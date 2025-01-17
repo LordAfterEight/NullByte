50628216d1630331667a50119a7155da8c821234
LordAfterEight <elias@stettmayer.de>
2025-01-17 09:41:14 +0100

    Changed project structure to be more practical and organized: - Added menus.rs - Added definitions.rs
    - Moved main_menu function over to menus.rs
    
    - Added imports for menus.rs and definitions.rs in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::{io,thread,time};
+mod definitions;
+mod menus;
+use definitions::*;
+use menus::*;
 
 use ratatui::{
     crossterm::event::{self, KeyCode, KeyEventKind},
@@ -16,44 +20,3 @@ fn main() -> io::Result<()> {
     app_result
 }
 
-fn main_menu(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let menu_ui = Paragraph::new("
-            Build: 17.01.2025, made by Elias Stettmayer
-
-            ┏━━━━━━━━━━━━━━━━━━━━━━ CLI-Miner ━━━━━━━━━━━━━━━━━━━━━┓
-            ┃ Exit [q]                                             ┃             
-            ┃                                                      ┃
-            ┃           Press [Enter] to start the game            ┃
-            ┃                                                      ┃
-            ┃           Press [e] to open settings                 ┃
-            ┃                                                      ┃
-            ┃                                                      ┃
-            ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-            ");
-            let menu_display = menu_ui
-                .white()
-                .on_black();
-            frame.render_widget(menu_display, frame.area());
-        })?;
-
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(());
-            }
-
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('e') {
-                println!("Settings opened");
-                thread::sleep(time::Duration::from_millis(1000));
-                return Ok(());
-            }
-
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('\n') {
-                println!("Enter pressed");
-                thread::sleep(time::Duration::from_millis(1000));
-                return Ok(());
-            }
-        }
-    }
-}
