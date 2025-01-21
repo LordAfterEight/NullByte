0e93ae0c86bcaeecc87527996c78f24cf50dfa2d
LordAfterEight <elias@stettmayer.de>
2025-01-21 10:16:56 +0100

    Changed program flow

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,43 @@
 use std::io;
 mod definitions;
 mod menus;
-use definitions::*;
 use menus::*;
+use definitions::*;
+use std::fs::File;
+use clearscreen;
 
-//use ratatui::{
-//    crossterm::event::{self, KeyCode, KeyEventKind},
-//    style::Stylize,
-//    widgets::Paragraph,
-//    DefaultTerminal,
-//};
+use ratatui::{crossterm::event::{self, KeyCode, KeyEventKind},
+    DefaultTerminal,
+    style::Stylize,
+    widgets::Paragraph,
+};
 
 fn main() -> io::Result<()> {
     let mut settings_mode = false;
     let mut terminal = ratatui::init();
     terminal.clear()?;
     let app_result = main_menu(terminal);
-    while(settings_mode) {
-    }
     ratatui::restore();
-    app_result
+    app_result;
+
+    if let event::Event::Key(key) = event::read()? {
+        if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q')  {
+            return Ok(());
+        }
+        if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('e')  {
+        }
+        if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('\n') {
+        }
+
+    }
+
+    while(true) {
+        println!("
+            Bits: 
+            
+            Miners:
+        ");
+        clearscreen::clear();
+    } Ok(())
 }
 
--- src/menus.rs
+++ src/menus.rs
@@ -1,56 +1,35 @@
-use std::{io,thread,time};
 use crate::definitions::*;
 use ratatui::{
     crossterm::event::{self, KeyCode, KeyEventKind},
     style::Stylize,
-    widgets::Paragraph,                                                                    DefaultTerminal,
+    widgets::Paragraph,
+    DefaultTerminal,
 };
 
-pub fn main_menu(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let menu_ui = Paragraph::new("
-            Build: 17.01.2025, made by Elias Stettmayer
-
-            ┏━━━━━━━━━━━━━━━━━━━━━━ CLI-Miner ━━━━━━━━━━━━━━━━━━━━━┓
-            ┃ Exit [q]                                             ┃
-            ┃                                                      ┃
-            ┃           Press [Enter] to start the game            ┃
-            ┃                                                      ┃
-            ┃           Press [e] to open settings                 ┃
-            ┃                                                      ┃
-            ┃                                                      ┃
-            ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-            ");
-            let menu_display = menu_ui
-                .white()
-                .on_black();
-            frame.render_widget(menu_display, frame.area());
-        })?;
-
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(());
-            }
-
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('e')
-            {
-                println!("Settings opened");
-            }
-
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('\n') {
-                println!("Enter pressed");
-                sleep(1000);
-                return Ok(());
-            }
-        }
-    }
+pub fn main_menu(mut terminal: DefaultTerminal) {
+    terminal.draw(|frame| {
+        let menu_ui = Paragraph::new("
+        Build: 17.01.2025, made by Elias Stettmayer
+        ┏━━━━━━━━━━━━━━━━━━━━━━ CLI-Miner ━━━━━━━━━━━━━━━━━━━━━┓
+        ┃ Exit [q]                                             ┃
+        ┃                                                      ┃
+        ┃           Press [Enter] to start the game            ┃
+        ┃                                                      ┃
+        ┃           Press [e] to open settings                 ┃
+        ┃                                                      ┃
+        ┃                                                      ┃
+        ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+        ");
+        let menu_display = menu_ui
+            .white()
+            .on_black();
+        frame.render_widget(menu_display, frame.area());
+    });
 }
 
-pub fn settings_menu(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let menu_ui = Paragraph::new("
+pub fn settings_menu(mut terminal: DefaultTerminal) {
+    terminal.draw(|frame| {
+        let menu_ui = Paragraph::new("
             Build: 17.01.2025, made by Elias Stettmayer
 
             ┏━━━━━━━━━━━━━━━━━━━━━━ Settings ━━━━━━━━━━━━━━━━━━━━━━┓
@@ -63,16 +42,13 @@ pub fn settings_menu(mut terminal: DefaultTerminal) -> io::Result<()> {
             ┃                                                      ┃
             ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
             ");
-            let menu_display = menu_ui
-                .white()
-                .on_black();
-            frame.render_widget(menu_display, frame.area());
-        })?;
+        let menu_display = menu_ui
+            .white()
+            .on_black();
+        frame.render_widget(menu_display, frame.area());
+    });
+}
 
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(());
-            }            
-        }
-    }
+pub fn game() {
+    todo!();
 }
