4bb4e3aeb4f9773a78edfb402982d9e597af7eb1
LordAfterEight <elias@stettmayer.de>
2025-01-29 22:47:02 +0100

    FINALLY got audio playback to work properly using rodio

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::io;
+use std::io::BufReader;
+use std::fs::File;
 mod definitions;
 mod screens;
 use screens::*;
 mod gamedata;
 use gamedata::*;
-
+use rodio::decoder::DecoderError;
+use rodio::{Decoder, OutputStream, source::Source, Sink};
 use ratatui::crossterm::event::{self, KeyCode, KeyEventKind};
+
+fn get_source(filename: &str) -> Decoder<BufReader<File>> {
+    let source = Decoder::new(BufReader::new(File::open(format!("../sound/{filename}")).unwrap()));
+    return source.unwrap()
+}
 
 fn main() -> io::Result<()> {
     let player = &mut Player {
@@ -17,34 +25,46 @@ fn main() -> io::Result<()> {
         miner_price: 60.0,
         converters: 0
     };
-    let state = &mut Gamestate { state: "Start Game".to_string() };
-    let mut frame_delay = 15;
+
+
+    let state = &mut Gamestate {state: "Start Game".to_string()};
+    let mut frame_delay = 50;
     let mut game_screen = false;
     let mut main_menu_screen = true;
     let mut settings_menu_screen = false;
     let mut terminal = ratatui::init();
 
-    definitions::play_mp3("music2.mp3");
-
+    let (_stream,stream_handle) = OutputStream::try_default().unwrap();
+    let sink_music = Sink::try_new(&stream_handle).unwrap();
+    let sink_sfx = Sink::try_new(&stream_handle).unwrap();
+    sink_sfx.append(get_source("interact.mp3"));
+    sink_music.append(get_source("music2.mp3"));
+    sink_sfx.sleep_until_end();
+    
     loop {
         let _ = terminal.clear();
         while main_menu_screen==true {
             render_main_menu(&mut terminal, state.state.clone());
-
             if let event::Event::Key(key) = event::read()? {
 
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q')  {
+                    sink_sfx.append(get_source("interact.mp3"));
+                    definitions::sleep(200);
                     ratatui::restore();
                     return Ok(());
                 }
 
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('e')  {
+                    sink_sfx.append(get_source("interact.mp3"));
                     main_menu_screen = false;
                     settings_menu_screen = true;
                     break;
                 }
 
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Enter {
+                    sink_sfx.append(get_source("interact.mp3"));
+                    sink_music.stop();
+                    sink_music.append(get_source("music1.mp3"));
                     state.state="Back to Game".to_string();
                     main_menu_screen = false;
                     game_screen = true;
@@ -60,17 +80,22 @@ fn main() -> io::Result<()> {
             if let event::Event::Key(key) = event::read()? {
 
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q')  {
+                    sink_sfx.append(get_source("interact.mp3"));
                     settings_menu_screen = false;
                     main_menu_screen = true;
                     break;
                 }
 
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('+')  {
+                    sink_sfx.stop();
+                    sink_sfx.append(get_source("interact.mp3"));
                     frame_delay += 1;
                     continue;
                 }
 
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('-') && frame_delay>1  {
+                    sink_sfx.stop();
+                    sink_sfx.append(get_source("interact.mp3"));
                     frame_delay -= 1;
                     continue;
                 }
@@ -90,18 +115,24 @@ fn main() -> io::Result<()> {
             if let event::Event::Key(key) = event::read()? {
 
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q')  {
+                    sink_sfx.append(get_source("interact.mp3"));
+                    sink_music.stop();
+                    sink_music.append(get_source("music2.mp3"));
                     game_screen = false;
                     main_menu_screen = true;
                     break;
                 }
 
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('e')  {
+                    sink_sfx.append(get_source("interact.mp3"));
                     main_menu_screen = false;
                     settings_menu_screen = true;
                     break;
                 }
 
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('1')  {
+                    sink_sfx.stop();
+                    sink_sfx.append(get_source("mining.mp3"));
                     player.bits += 1*&player.miners;
                 }
 
