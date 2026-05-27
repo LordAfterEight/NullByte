9934a57a1e35e28b0793b7d00bb312b918a3efaf
LordAfterEight <elias@stettmayer.de>
2026-05-27 22:19:49 +0200

    Update dependencies and refactor main application logic

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 use NullByte;
 use NullByte::core::{Game, player};
 use NullByte::devices;
-use winit_ui::font;
 use discord_rich_presence::DiscordIpc;
+use winit_ui::font;
 
 const WHITE: winit_ui::color::Color = winit_ui::color::Color::new(0xFF, 0xFF, 0xFF, 0xFF);
 const BLACK: winit_ui::color::Color = winit_ui::color::Color::new(0x00, 0x00, 0x00, 0xFF);
@@ -15,36 +15,21 @@ const TEAL: winit_ui::color::Color = winit_ui::color::Color::new(0x24, 0xEB, 0xC
 
 fn main() {
     let settings = NullByte::core::settings::Settings::load_or_default("settings.toml");
-
-    let mut app = winit_ui::App::new(
-        "NullByte",
-        settings.window.width,
-        settings.window.height,
-        settings.window.resizable,
-        settings.window.fullscreen,
-    );
-
     let mut game = Game::init(settings);
 
     game.player = Some(player::Player::new("Default Player Name"));
-    game.player
-        .as_mut()
-        .unwrap()
-        .add_device(devices::miner::Miner::new(0));
-
-    game.audio_manager.new_channel(
-        "assets/sound/NullByte [Main Menu Theme].wav",
-        "Music",
-    );
+    game.audio_manager
+        .new_channel("assets/sound/NullByte [Main Menu Theme].wav", "Music");
     game.audio_manager
         .set_next("Music", "assets/sound/NullByte [Main Menu Theme].wav");
-    
     game.audio_manager.new_channel(
         "assets/sound/NullByte Computer Ambience Start.wav",
         "Ambience",
     );
     game.audio_manager
         .set_next("Ambience", "assets/sound/NullByte Computer Ambience.wav");
+    game.instant_start = std::time::Instant::now();
+    game.update();
 
     let fonts = vec![
         winit_ui::font::Font::new("assets/fonts/IBMPlexMono-Light.ttf"),
@@ -52,26 +37,31 @@ fn main() {
         winit_ui::font::Font::new("assets/fonts/IBMPlexMono-Bold.ttf"),
     ];
 
+    let app = winit_ui::App::new("NullByte", game.settings.window.width, game.settings.window.height)
+        .keep_aspect_ratio(true)
+        .set_resizable(true)
+        .on_draw(move |canvas| {
+            titlescreen(canvas, &mut game, &fonts);
+            game.update();
+        });
+
     let mut discord_client = discord_rich_presence::DiscordIpcClient::new("1424335981036834887");
     discord_client.connect().ok();
     _ = discord_client.set_activity(
         discord_rich_presence::activity::Activity::new()
-        .name("NullByte")
-        .details("In Main Menu")
-        .activity_type(discord_rich_presence::activity::ActivityType::Playing)
+            .name("NullByte")
+            .details("In Main Menu")
+            .activity_type(discord_rich_presence::activity::ActivityType::Playing),
     );
-    
-    game.instant_start = std::time::Instant::now();
-    game.update();
 
-    app.on_draw(move |canvas| {
-        titlescreen(canvas, &mut game, &fonts);
-        game.update();
-    });
     app.run();
 }
 
-fn titlescreen(canvas: &mut winit_ui::canvas::Canvas, game: &mut Game, fonts: &Vec<winit_ui::font::Font>) {
+fn titlescreen(
+    canvas: &mut winit_ui::canvas::Canvas,
+    game: &mut Game,
+    fonts: &Vec<winit_ui::font::Font>,
+) {
     let font_light = &fonts[0];
     let font_regular = &fonts[1];
     let font_bold = &fonts[2];
@@ -102,7 +92,7 @@ fn titlescreen(canvas: &mut winit_ui::canvas::Canvas, game: &mut Game, fonts: &V
         canvas.height / 4 + 30,
         canvas.width / 2 + winit_ui::get_text_width("NullByte", 96.0, font_regular) / 2 + 100,
         canvas.height / 4 + 30,
-        &DARK_GRAY
+        &DARK_GRAY,
     );
 
     // ========== Draw blinking cursor ===========
